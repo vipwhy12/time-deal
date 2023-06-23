0e69993d46d2ac5ac523661fc273ea193fdf3f97
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-06-24 05:39:56 +0900

    [feat] : Add ValidationPipe
    import class-transformer, validator 그리고 전역 범위 파이프 설정

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 
 const PORT = 8080;
@@ -7,6 +8,13 @@ const handleListening = () => console.log(`✅ Server listenting on port ${PORT}
 
 async function serverListen() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist : true,
+    forbidNonWhitelisted : true,
+    transform: true  
+  }))
+
+
   await app.listen(PORT, handleListening);
 }
 serverListen();
