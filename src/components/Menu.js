49faf1a95416bd97ff938b7516bff65c4d534913
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-06-30 04:14:25 +0900

    [feat] : nav-var 카테고리 href 수정
    href 이동 url 수정

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,13 +2,19 @@ import {Container, Nav, Navbar} from 'react-bootstrap';
 
 function Menu(){
   return(
+    <>
     <div className='nav-bar'>
     <Navbar bg="black" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">NoTag</Navbar.Brand>
+        <Navbar.Brand href="/">NoTag</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/brands">Brand</Nav.Link>
+          <Nav.Link href="/products">Product</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
-  </div>
+    </div>
+    </>
   )
 }
 
