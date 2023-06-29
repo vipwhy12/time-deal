f7e51695c086b9b1f7117402643b14c89824c549
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-06-30 04:13:40 +0900

    [feat] : Product 상품 전체 조회 생성

---
+++ src/routes/Product.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+
+export default function Product(){
+  const [products, setProduct] = useState(null)
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products").then((response) => {
+      setProduct(response.data);
+      console.log(response.data)
+    });
+  }, []);
+
+
+  if (!products) return (
+    <Spinner animation="border" variant="dark" />
+  )
+
+  return(
+    <>
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th></th>
+          <th>상품</th>
+          <th>브랜드</th>
+          <th>카테고리</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product, index) => {
+          return(
+            <tr key={product.id}>
+            <td>{index + 1}</td>
+            <td>{product.name}</td>
+            <td>{product.brand.name}</td>
+            <td>{product.category[0].name}</td>
+          </tr>
+
+          )
+          })}
+      </tbody>
+    </Table>
+
+    </>
+  )
+}
