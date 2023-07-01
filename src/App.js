e45afb5e6e7e4d0d844363f8d5a3399682f2add5
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-01 14:44:40 +0900

    [feat] : 제품 상세 페이지, 랭크 Route 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './css/App.css'
 import Home from './routes/Home';
 import Brand from './routes/Brand'
 import Product from './routes/Product'
+import Rank  from './routes/Rank';
 import Category from './routes/Category';
 import BrandDetail from './routes/BrandDetail'
+import ProductDetail from './routes/ProductDetail'
 
 import Menu from './components/Menu';
 
@@ -17,15 +19,17 @@ export default function App() {
         <div className='main-screen'>
         <Routes>
           <Route path='/' element={<Home/>}/>
+
           <Route path='/brands' element={<Brand/>}/>
           <Route path='/brands/:id' element= {<BrandDetail/>}/>
+
           <Route path='/category' element={<></>}/>
           <Route path='/category/:id' element={<Category></Category>}/>
 
-
-
           <Route path='/products' element={<Product/>}/>     
-          <Route path='/chart' element={<div></div>}/>
+          <Route path='/products/:id' element={<ProductDetail/>}/>
+          
+          <Route path='/rank' element={<Rank></Rank>}/>
           <Route path='*' element= {<div> 없는 페이지입니다. </div>}></Route>
         </Routes>
         </div>
