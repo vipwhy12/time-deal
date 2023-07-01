0bade473a0ac0cce40fb6a0b49e9240d81f516b9
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-01 14:36:49 +0900

    [refector] : href ->  navigate
     href ->  navigate로 통일

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,18 @@
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
-function Menu(){
+export default function Menu(){
+  const navigate = useNavigate()
+
   return(
     <>
     <div className='nav-bar'>
     <Navbar bg="black" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">NoTag</Navbar.Brand>
+        <Navbar.Brand onClick={() => {navigate('')}}>NoTag</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/brands">Brand</Nav.Link>
-          <Nav.Link href="/products">Product</Nav.Link>
+          <Nav.Link onClick={() => {navigate('/brands')}}>Brand</Nav.Link>
+          <Nav.Link onClick={() => {navigate('/products')}}>Product</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
@@ -17,6 +20,3 @@ function Menu(){
     </>
   )
 }
-
-
-export default Menu;
