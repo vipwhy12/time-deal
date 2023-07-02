1eab5ee48e82e59368e2653974eff91b6eab622f
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-03 04:32:49 +0900

    [refactor] : ♻️NotFount 시맨틱태그 적용

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return(
-    <div>
-      <title>Not Found</title>
-      <h2>Page Not Found.</h2>
-      <h4>🙇🏻‍♀️찾으려는 페이지가 없거나 이동했습니다.🙇🏻‍♀️</h4>
-      <Link to="/">🏠메인페이지로 돌아가기! &rarr;🏠</Link>
-  </div>
+    <>
+      <header>
+        <title>Not Found</title>
+        <h2>Page Not Found.</h2>
+      </header>
+      <main>
+        <h4>🙇🏻‍♀️찾으려는 페이지가 없거나 이동했습니다.🙇🏻‍♀️</h4>
+        <Link to="/">🏠메인페이지로 돌아가기!🏠</Link>
+      </main>
+    </>
   )
 }
