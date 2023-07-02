7fd920aa391a3507373ac6fd1f683212de899eb5
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-03 01:52:25 +0900

    [fix] : 🚚routes 폴더명 변경
    routes 폴더명 pages로 변경

---
+++ src/pages/BrandCreate.js
@@ -0,0 +1,16 @@
+
+
+export default function BandCreate(){
+  return(
+    <>
+      <div>
+        <h2>브랜드 이름</h2>
+        <input type="text"></input>
+        
+        <h3>브랜드 설명</h3>
+        <input type="text"></input>
+
+      </div>
+    </>
+  )
+};
