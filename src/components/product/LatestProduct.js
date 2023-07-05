1e6e16fdd6c076458bf2c25fc8aa66b06b47e1c2
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-06 02:56:21 +0900

    [feat] : ✨최신 브랜드, 최신 상품 생성✨

--- src/components/product/LatestProduct.js
+++ src/components/product/LatestProduct.js
@@ -1,15 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
-export default function LatestBrand({brands}){
+export default function LatesProducts({products}){
   const navigate = useNavigate()
 
   return(
     <article>
-      <h3>최신 브랜드</h3>
+      <h3>최신 상품</h3>
       <div>
-        {brands.map(({id, name}, index)=>{
+        {products.map(({id, name}, index)=>{
             return(
-                <div key={id} onClick={() => {navigate('/brands/' + id)}}>
+                <div key={id}>
                   <h5>{index + 1}. {name}</h5>
                 </div>
               )
