3741d75b488a1740414360ee62899a531798c9b7
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-07 06:33:02 +0900

    [refactor] : ♻️Product List 컴포넌트로 대체

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -1,51 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Spinner from 'react-bootstrap/Spinner';
-import axios from 'axios';
-import Table from 'react-bootstrap/Table';
-import { useNavigate } from 'react-router-dom';
+import ProductList from "../../components/product/ProductList";
+import React, { useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-
-export default function Products(){
-  const [products, setProduct] = useState(null)
-  const navigate = useNavigate()
+export default function Products() {
+  const [products, setProduct] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:8080/products").then((response) => {
       setProduct(response.data);
-      console.log(response.data)
     });
   }, []);
 
+  if (!products) return <Spinner animation="border" variant="dark" />;
 
-  if (!products) return (
-    <Spinner animation="border" variant="dark" />
-  )
-
-  return(
-    <>
-      <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th></th>
-          <th>상품</th>
-          <th>브랜드</th>
-          <th>카테고리</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map((product, index) => {
-          return(
-            <tr key={product.id} onClick={() => { navigate('/products/' + product.id) }}>
-            <td>{index + 1}</td>
-            <td>{product.name}</td>
-            <td>{product.brand.name}</td>
-            <td>{product.category[0].name}</td>
-          </tr>
-          )
-          })}
-      </tbody>
-    </Table>
-
-    </>
-  )
+  return (
+    <main>
+      <ProductList products={products} />
+    </main>
+  );
 }
