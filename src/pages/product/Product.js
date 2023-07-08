185309a81781055276c1790f524642b7ff9f492e
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-09 07:41:00 +0900

    [feat] : Product CSS적용

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,29 +1,59 @@
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Spinner from 'react-bootstrap/Spinner';
-import axios from 'axios';
+import Spinner from "react-bootstrap/Spinner";
+import axios from "axios";
+import React from "react";
+import styled from "styled-components";
+import ListGroup from "react-bootstrap/ListGroup";
 
-export default function Product(){
-  const {id} = useParams();
-  const [product, setProduct] = useState(null)
-  const [brand, setBrand] = useState(null)
+export default function Product() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [category, setCategory] = useState(null);
+  const img =
+    "https://static.wixstatic.com/media/a442d9_1a2ce063dbe94e7e91cd43e4dd22088d~mv2.jpg/v1/fit/w_2500,h_1330,al_c/a442d9_1a2ce063dbe94e7e91cd43e4dd22088d~mv2.jpg";
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products/" + id).then((response) => {
-      setProduct(response.data);
-      setBrand(response.data.brand)
+    axios.get("http://localhost:8080/products/" + id).then(({ data }) => {
+      setProduct(data);
+      axios
+        .get("http://localhost:8080/category/ancestors/" + data.category[0].id)
+        .then(({ data }) => {
+          setCategory(data);
+        });
     });
-  }, []);
+  }, [id]);
 
-  if (!product && !brand) return (
-    <Spinner animation="border" variant="dark" />
-  )
+  if (!product || !category)
+    return <Spinner animation="border" variant="dark" />;
 
-  return(
-    <>
-    <h1>{product.name}</h1>
-    <h1>{brand.name}</h1>
-    </>
-  )
-
+  return (
+    <Container>
+      <ProductImage src={img} />
+      <h1>{product.name}</h1>
+      <ListGroup>
+        <ListGroup.Item>👉 브랜드 : {product.brand.name}</ListGroup.Item>
+        <ListGroup.Item>
+          👉 카테고리 :{" "}
+          {product.category.map(({ name }) => {
+            return <> {name} ,</>;
+          })}
+        </ListGroup.Item>
+      </ListGroup>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const ProductImage = styled.img`
+  width: 200px;
+  height: auto;
+  border-radius: 10px;
+  margin-bottom: 10px;
+`;
--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -1,24 +1,83 @@
 import ProductList from "../../components/product/ProductList";
+import CategorySelect from "../../components/category/CategorySelect";
 import React, { useEffect, useState } from "react";
-import Spinner from "react-bootstrap/Spinner";
 import axios from "axios";
+import Loading from "../../components/Loading";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Products() {
   const [products, setProduct] = useState(null);
+  const [rootCategory, setRootCategory] = useState(null);
+  const [userSelect, setUserSelect] = useState(null);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products").then((response) => {
-      setProduct(response.data);
-    });
-  }, []);
+    const loadData = async () => {
+      try {
+        const productsResponse = await axios.get(
+          "http://localhost:8080/products"
+        );
+        setProduct(productsResponse.data);
 
-  if (!products) return <Spinner animation="border" variant="dark" />;
+        const rootCategoryResponse = await axios.get(
+          `http://localhost:8080/category/root`
+        );
+        setRootCategory(rootCategoryResponse.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("🥲데이터를 불러오는 데 실패했습니다.");
+        console.error("err: ", error.message);
+      }
+    };
+
+    const loadProducts = async (id) => {
+      try {
+        const productDescendants = await axios.get(
+          `http://localhost:8080/category/descendants/${id}`
+        );
+        setProduct(productDescendants.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("🥲데이터를 불러오는 데 실패했습니다.");
+        console.error("err: ", error.message);
+      }
+    };
+
+    if (!userSelect) {
+      loadData();
+    } else {
+      console.log(userSelect);
+      setUserSelect(userSelect);
+      loadProducts(userSelect);
+    }
+  }, [userSelect]);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <main>
-      <ProductList products={products} />
+      <Title>
+        <h2>📍Product List</h2>
+      </Title>
+      <CategorySelect
+        rootCategory={rootCategory}
+        setUserSelect={setUserSelect}
+      />
+      <ProductList
+        products={products}
+        userSelect={userSelect}
+        setUserSelect={setUserSelect}
+      />
     </main>
   );
 }
+
+const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 5%;
+`;
