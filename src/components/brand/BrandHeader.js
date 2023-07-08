e279a2cd3f8a9b2534e61458122d19927a7f9a29
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-09 00:10:20 +0900

    [feat] : BrandHeader CSS 적용

--- src/components/brand/BrandHeader.js
+++ src/components/brand/BrandHeader.js
@@ -1,47 +1,43 @@
-import { Col, Row, Image, Button } from "react-bootstrap";
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import styled from "styled-components";
 
-export default function BrandHeader({ brand }) {
+const BrandHeader = ({ brand }) => {
   const img =
     "https://static.wixstatic.com/media/a442d9_1a2ce063dbe94e7e91cd43e4dd22088d~mv2.jpg/v1/fit/w_2500,h_1330,al_c/a442d9_1a2ce063dbe94e7e91cd43e4dd22088d~mv2.jpg";
 
   return (
-    <Row style={{ height: "30vh" }}>
-      <Col
-        xs={2}
-        className="justify-content-center align-items-center"
-        style={{ display: "flex" }}
-      >
-        <div>
-          <Image
-            src={img}
-            roundedCircle
-            style={{ width: "100%", zIndex: "3" }}
-          ></Image>
-        </div>
-      </Col>
+    <Header>
+      <Row>
+        <Col xs={3}>
+          <ImageContainer src={img} />
+        </Col>
 
-      <Col xs={7} className="align-items-center" style={{ display: "flex" }}>
-        <div
-          className=""
-          style={{ background: "", display: "flex", flexDirection: "column" }}
-        >
-          <h1>{brand.name}</h1>
-          <span>{brand.description}</span>
-        </div>
-      </Col>
-
-      <Col className="d-flex flex-row align-items-center">
-        <div style={{ width: "100%" }}>
-          <div className="d-flex flex-column gap-3 justify-content-center">
-            <Button variant="dark" size="lg" style={{ width: "100%" }}>
-              브랜드 수정
-            </Button>
-            <Button variant="dark" size="lg" style={{ width: "100%" }}>
-              브랜드 삭제
-            </Button>
-          </div>
-        </div>
-      </Col>
-    </Row>
+        <Col xs={5}>
+          <BrandContainer>
+            <h1>{brand.name}</h1>
+            <span>{brand.description}</span>
+          </BrandContainer>
+        </Col>
+      </Row>
+    </Header>
   );
-}
+};
+
+const Header = styled.header`
+  padding: 20px;
+`;
+
+const ImageContainer = styled.img`
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  object-fit: cover;
+  zindex: 3;
+`;
+
+const BrandContainer = styled.div`
+  margin-top: 30px;
+`;
+
+export default BrandHeader;
