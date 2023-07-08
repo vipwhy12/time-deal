24c11f927b35022f4466cf99c89cb51133558d83
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-08 19:35:33 +0900

    [feat] : CSS 완료

--- src/components/LatestData.js
+++ src/components/LatestData.js
@@ -1,26 +1,31 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import { useNavigate } from "react-router-dom";
 
 export default function LatestData({ data, title, url }) {
   const navigate = useNavigate();
+  const move = (moveData) => {
+    navigate(url + moveData.id);
+  };
 
   return (
-    <article>
-      <h3>{title}</h3>
-      <div>
-        {data &&
-          data.map(({ id, name }, index) => (
-            <div
+    <Card style={{ width: "49%" }}>
+      <Card.Header>{title}</Card.Header>
+      <ListGroup variant="flush">
+        {data.map(({ id, name }, index) => {
+          return (
+            <ListGroup.Item
               key={id}
               onClick={() => {
-                navigate(url + id);
+                move({ id });
               }}
             >
-              <h5>
-                {index + 1}. {name}
-              </h5>
-            </div>
-          ))}
-      </div>
-    </article>
+              {index + 1}. {name}
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+    </Card>
   );
 }
