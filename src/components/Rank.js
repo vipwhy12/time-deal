fd921c30538eec5ae3fb45133055667835837631
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-07-08 19:50:37 +0900

    [feat] : Rank CSS 적용

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,15 +1,42 @@
-export default function Rank({rank}){
+import { Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
+import styled from "styled-components";
 
-  return(
-    <>
-      {rank && rank.map(({brandName, total}, index) => {
-        return (
-          <div key={brandName}>
-            {brandName}
-            {total}
-          </div>
-        )
-      })}
-    </>
-  )
+export default function Rank({ rank }) {
+  const navigate = useNavigate();
+  const move = (data) => {
+    navigate("/sales/brands/" + data.brandId);
+  };
+
+  return (
+    <Card>
+      <Card.Header>🔥브랜드 판매 순위🔥</Card.Header>
+      <ListGroup variant="flush">
+        {rank &&
+          rank.map(({ brandId, brandName, total }, index) => {
+            return (
+              <ListGroup.Item
+                key={brandName}
+                onClick={() => {
+                  move({ brandId });
+                }}
+              >
+                <ItemCountainer>
+                  <div>
+                    {index + 1}. {brandName}
+                  </div>
+                  <div>판매량 : {total}</div>
+                </ItemCountainer>
+              </ListGroup.Item>
+            );
+          })}
+      </ListGroup>
+    </Card>
+  );
 }
+
+const ItemCountainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
